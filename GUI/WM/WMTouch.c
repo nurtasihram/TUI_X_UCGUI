a251258636583fe5e32418cfa28eedc16e21cd0d
Nurtas Ihram <65583223+nurtasihram@users.noreply.github.com>
2026-07-04 21:16:20 +0500

    #
    重構 WM_HandlePID，統一觸控狀態處理
    
    重構 WM_HandlePID 函式，移除未使用的 State 變數，統一以 StateNew 儲存與傳遞觸控狀態。修正 pState 判斷條件，將 WM__CHWinLast.hWin 由 0 改為 NULL，並統一傳遞 StateNew 至 _SendTouchMessage，提升語意與可維護性。補充狀態同步，確保觸控事件正確處理。

--- GUI/WM/WMTouch.c
+++ GUI/WM/WMTouch.c
@@ -53,9 +53,8 @@ static WM_HWIN _Screen2Win(GUI_PID_STATE *pState) {
 int WM_HandlePID(void) {
 	int r = 0;
 	WM_CRITICAL_HANDLE CHWin;
-	GUI_PID_STATE State, StateNew;
+	GUI_PID_STATE StateNew;
 	GUI_PID_GetState(&StateNew);
-
 	WM__AddCriticalHandle(&CHWin);
 	if ((WM_PID__StateLast.x != StateNew.x) || (WM_PID__StateLast.y != StateNew.y) || (WM_PID__StateLast.Pressed != StateNew.Pressed)) {
 #if GUI_SUPPORT_CURSOR
@@ -79,7 +78,7 @@ int WM_HandlePID(void) {
 			 * Send WM_TOUCH message(s)
 			 * Note that we may have to send 2 touch messages.
 			 */
-			if (WM_PID__StateLast.Pressed | StateNew.Pressed) {    /* Only if pressed or just released */
+			if (WM_PID__StateLast.Pressed | StateNew.Pressed) { /* Only if pressed or just released */
 				r = 1;
 				/*
 				 * Tell window if it is no longer pressed
@@ -89,39 +88,36 @@ int WM_HandlePID(void) {
 				 */
 				if (WM__CHWinLast.hWin != CHWin.hWin) {
 					if (WM__CHWinLast.hWin) {
-						GUI_PID_STATE *pState = WM_PID__StateLast.Pressed ? &WM_PID__StateLast : NULL;
 						GUI_DEBUG_LOG("\nSending WM_Touch to LastWindow %d (out of area)", WM__CHWinLast.hWin);
+						GUI_PID_STATE *pState = StateNew.Pressed ? NULL : &WM_PID__StateLast;
 						_SendTouchMessage(WM__CHWinLast.hWin, WM_TOUCH, pState);
-						WM__CHWinLast.hWin = 0;
+						WM__CHWinLast.hWin = NULL;
 					}
 				}
 				/* Sending WM_Touch to current window */
 				if (CHWin.hWin) {
-					/* convert screen into window coordinates */
-					State = StateNew;
 					/* Remember window */
-					if (State.Pressed)
+					if (StateNew.Pressed)
 						WM__CHWinLast.hWin = CHWin.hWin;
 					else {
 						/* Handle automatic capture release */
 						if (WM__CaptureReleaseAuto)
 							WM_ReleaseCapture();
-						WM__CHWinLast.hWin = 0;
+						WM__CHWinLast.hWin = NULL;
 					}
-					_SendTouchMessage(CHWin.hWin, WM_TOUCH, &State);
+					_SendTouchMessage(CHWin.hWin, WM_TOUCH, &StateNew);
 				}
 			}
 #if GUI_SUPPORT_MOUSE
 			/* Send WM_MOUSEOVER Message */
 			else if (CHWin.hWin)
 				/* Do not send messages to disabled windows */
-				if (WM_IsEnabled(CHWin.hWin)) {
-					State = StateNew;
-					_SendTouchMessage(CHWin.hWin, WM_MOUSEOVER, &State);
-				}
+				if (WM_IsEnabled(CHWin.hWin))
+					_SendTouchMessage(CHWin.hWin, WM_MOUSEOVER, &StateNew);
 #endif
 		}
 		/* Store the new state */
+		GUI_PID_GetState(&StateNew);
 		WM_PID__StateLast = StateNew;
 	}
 	WM__RemoveCriticalHandle(&CHWin);
